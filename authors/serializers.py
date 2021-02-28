7c051c54ff585108f80f55be4f9e080b573efb40
Tippman <gl.8700@hmail.com>
2021-02-28 14:14:57 +0300

    hm_1

---
+++ authors/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework.serializers import HyperlinkedModelSerializer
+from authors.models import Author
+
+
+class AuthorModelSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = Author
+        fields = '__all__'
+
+
